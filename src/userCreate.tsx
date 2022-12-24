631b39988443e0946c467e6aef38e6ffc54d34eb
syed1RehanAhmed <syed@ampcome.com>
2022-12-24 21:30:25 +0530

    mutation

--- src/userCreate.tsx
+++ src/userCreate.tsx
@@ -5,6 +5,7 @@ import {
   Form,
   Input,
   Row,
+  Space,
   Table,
   useForm,
   useTable,
@@ -32,15 +33,22 @@ export const UserCreate: React.FC = () => {
     <>
       <Form
         {...formProps}
-        onFinish={(values) => {
+        onFinish={(values: any) => {
+          const {
+            permanent_address,
+            optional_address,
+            pincode1,
+            pincode2,
+            ...rest
+          } = values;
           return (
             formProps.onFinish &&
             formProps.onFinish({
-              ...values,
+              ...rest,
               addresses: {
                 data: [
-                  { city: "testing1234", pincode: "787767" },
-                  { city: "testing343", pincode: "6767" },
+                  { city: permanent_address, pincode: pincode1 },
+                  { city: optional_address, pincode: pincode2 },
                 ],
               },
             })
@@ -60,6 +68,26 @@ export const UserCreate: React.FC = () => {
             </Form.Item>
           </Col>
         </Row>
+
+        <Space direction="horizontal">
+          <Form.Item name={"permanent_address"}>
+            <Input placeholder="permanent address" />
+          </Form.Item>
+          <Form.Item name={"pincode1"}>
+            <Input placeholder="pincode 1" />
+          </Form.Item>
+        </Space>
+        <Row>
+          <Space direction="horizontal">
+            <Form.Item name={"optional_address"}>
+              <Input placeholder="optional address" />
+            </Form.Item>
+            <Form.Item name={"pincode2"}>
+              <Input placeholder="pincode 2" />
+            </Form.Item>
+          </Space>
+        </Row>
+
         <Button {...saveButtonProps}>save</Button>
       </Form>
     </>
@@ -67,3 +95,25 @@ export const UserCreate: React.FC = () => {
 };
 
 export default UserCreate;
+
+// mutation MyMutation($data: [address_insert_input!] = [{city: "testing",pincode:"787767"}, {city: "testing2",pincode:"6767"}]) {
+//   insert_user_info(objects: {addresses: {data: $data}, date_of_birth: "7-9-2011", name: "ooooo", job: {data: {job_name: "bbbbb"}}}) {
+//     returning {
+//       addresses {
+//         city
+//       }
+//       name
+//     }
+//   }
+// }
+
+// mutation MyMutation2($data: job_insert_input = {job_name: "sales"}) {
+//   insert_user_info(objects: {name: "test2", job: {data: $data}}) {
+//     returning {
+//       name
+//       job {
+//         job_name
+//       }
+//     }
+//   }
+// }
